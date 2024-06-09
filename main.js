39eeb20eec506675397884b64a2b847a8e7845b4
wily <103911498+botreaction@users.noreply.github.com>
2024-06-09 21:50:27 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 process.on('uncaughtException', console.error);
-const { default: KUNTUL, useMultiFileAuthState, DisconnectReason, makeInMemoryStore, getContentType } = (await import('baileys')).default;
+const { default: KUNTUL, useMultiFileAuthState, PHONENUMBER_MCC, DisconnectReason, makeInMemoryStore, getContentType } = (await import('baileys')).default;
 import { Boom } from '@hapi/boom';
 import p from 'pino';
 import cfonts from 'cfonts';
 import cuy from './config.js';
 import { connect } from './server.js';
-const log = p({ level: 'silent' });
-const PORT = process.env.PORT || 3000  
+import chalk from "chalk"
+import NodeCache from 'node-cache'
 
+const log = p({ level: 'silent' });
+  
 cfonts.say('auto-read-sw\nby-wily-kun', {// Ubah saja cuii ;v
 	font: 'tiny',       
 	align: 'center',
@@ -45,9 +47,7 @@ const h = async (u, c) => {
 			setTimeout(() => {
 				c.readMessages([m.key])
 				let mt = getContentType(m.message)
-				if (!(mt.match(/protocolMessage/i))) {
-				console.log(`Melihat story user : ${m.key.participant.split('@')[0]}`)
-				}
+				console.log((/protocolMessage/i.test(mt)) ? `Telah Menghapus Story Nya User : ${m.key.participant.split('@')[0]}` : 'Telah Melihat Story Nya User : '+m.key.participant.split('@')[0]);
 			}, cuy.faston);
 		}
 	} catch (e) {
@@ -56,22 +56,58 @@ const h = async (u, c) => {
 }
 const start = async () => {
 	try {
-		const store = makeInMemoryStore({ logger: log })
+	      const store = makeInMemoryStore({ logger: p().child({ level: 'silent', stream: 'store' }) })
 		const { state, saveCreds } = await useMultiFileAuthState('WilyKun');
-		const client = KUNTUL({
-			browser: [cuy.name, 'safari', '1.0.0'],
-			printQRInTerminal: true,
-      markOnlineOnConnect: false,
+		const msgRetryCounterCache = new NodeCache()
+		const config = {
+			browser: ['Linux', 'Chrome', ''],
+            version: [2, 2413, 51],
+            pairingCode: true, 
+            markOnlineOnConnect: false,
 			logger: log,
-			auth: state
-		});
+			auth: state,
+			getMessage: async (key) => {
+            if (store) {
+                const msg = await store.loadMessage(key.remoteJid, key.id)
+                return msg.message || undefined
+            }
+            return {
+                conversation: "bot"
+	   }
+	},
+		      msgRetryCounterCache,
+		      connectTimeoutMs: 60000,
+    defaultQueryTimeoutMs: 0,
+    keepAliveIntervalMs: 10000,
+    emitOwnEvents: true,
+    fireInitQueries: true,
+    generateHighQualityLinkPreview: true,
+    syncFullHistory: true,
+    markOnlineOnConnect: true
+		}
+		let client = KUNTUL(config);
+		
+		if (!client.authState.creds.registered) {
+
+    let phoneNumber
+        phoneNumber = cuy.pairingNumber.replace(/[^0-9]/g, '')
+        if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
+            console.log(chalk.bgBlack(chalk.redBright("Mulailah dengan kode WhatsApp negara Anda, Contoh : 62xxx")))
+            process.exit(0)
+        }
+        
+        setTimeout(async () => {
+        let code = await client.requestPairingCode(phoneNumber)
+        code = code?.match(/.{1,4}/g)?.join("-") || code
+        console.log(chalk.yellow(chalk.bgRed(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
+}, 3000)
+}
 		store.bind(client.ev)
 		client.ev.on('connection.update', async (up) => j(up, client, start));
 		client.ev.on('messages.upsert', async (up) => h(up, client));
 		client.ev.on('creds.update', saveCreds);
-    connect(client, PORT)
 	} catch (e) {
-		console.log('');
+		console.log(e);
 	}
 };
 start()
